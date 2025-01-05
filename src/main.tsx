0cbdc6363d8158493cbb3bc74f23178c8cd09e4d
rstainsby <rstainsbydevelopment@gmail.com>
2025-01-05 21:17:23 +1000

    chore: install and configure TypeScript

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import React from "react";
+
+const App = () => {
+  return (
+    <StrictMode>
+      <div className="container">
+        <h1>Hello, World!</h1>
+      </div>
+    </StrictMode>
+  )
+};
+
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("No root element found");
+}
+
+const root = createRoot(container);
+root.render(React.createElement(App));
