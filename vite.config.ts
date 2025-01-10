c42f8a0c0dd1208623b669cacc2186711c3864ec
rstainsby <rstainsbydevelopment@gmail.com>
2025-01-10 16:54:09 +1000

    chore: install shadcn theming

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "src"),
+    }
+  }
 })
