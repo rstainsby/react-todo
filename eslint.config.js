20263929877f0a87459f9c285540d466dbc11489
rstainsby <rstainsbydevelopment@gmail.com>
2025-01-15 12:15:24 +1000

    fix: broken eslint defaults causing false flags

--- eslint.config.js
+++ eslint.config.js
@@ -46,6 +46,9 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+      "react-hooks/exhaustive-deps": "off",
       "react/jsx-no-target-blank": "off",
       "react-refresh/only-export-components": [
         "warn",
